d891d131f2062a2231622a4e02c89705a307b783
十百千 <x1001000@users.noreply.github.com>
2017-06-10 13:31:59 +0800

    Update bottle_8080.py

--- 1_IronMan/bottle_8080.py
+++ 1_IronMan/bottle_8080.py
@@ -13,21 +13,42 @@ def gogo(sentence):
         if word=='跑':
             control.move('w',1)
         elif word=='前':
-            control.move('w',0.3)
+            control.move('w',0.4)
         elif word=='進':
-            control.move('w',0.3)
+            control.move('w',0.4)
         elif word=='左':
-            control.move('a',0.3)
+            control.move('a',0.4)
         elif word=='右':
-            control.move('d',0.3)
+            control.move('d',0.4)
+        elif word=='後':
+            control.move('s',0.4)
         elif word=='圈':
             control.move('a',1)
-            sleep(1)
+            sleep(0.1)
             control.move('d',1)
 
 @route('/<x>')
 def movement(x):
     control.move(x)
+
+@route('/pwm/<ena>/<enb>')
+def movement_by_pwm(ena,enb):
+    if ena > 0:
+        control.GPIO.output(control.in1,1)
+        control.GPIO.output(control.in2,0)
+    else :
+        control.GPIO.output(control.in1,0)
+        control.GPIO.output(control.in2,1)
+    
+    if enb > 0:
+        control.GPIO.output(control.in3,1)
+        control.GPIO.output(control.in4,0)
+    else :
+        control.GPIO.output(control.in3,0)
+        control.GPIO.output(control.in4,1)
+    
+    control.ena.ChangeDutyCycle(abs(ena))
+    control.enb.ChangeDutyCycle(abs(enb))
 
 try:
     run(host='0.0.0.0', port=8080)
